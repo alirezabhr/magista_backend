2a9b3922fe112bf6d6279e83257ca169a89ebd54
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-05 13:56:36 +0330

    signup view created to create new user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer, ShopSerializer
+
 
 # Create your views here.
+class SignupUserView(APIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        ser = self.serializer_class(data=request.data)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        return Response(ser.data, status=status.HTTP_201_CREATED)
+
