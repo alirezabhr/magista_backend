d72f9c7c797fe11a8b67477c62927e45ea19bf8d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-06 00:12:52 +0330

    user signup, customer and shop registration added

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import UserSerializer, ShopSerializer
+from .models import Shop
+from .serializers import UserSerializer, ShopSerializer, CustomerSerializer
 
 
 # Create your views here.
@@ -11,7 +13,36 @@ class SignupUserView(APIView):
 
     def post(self, request):
         ser = self.serializer_class(data=request.data)
-        ser.is_valid(raise_exception=True)
-        ser.save()
-        return Response(ser.data, status=status.HTTP_201_CREATED)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomerView(APIView):
+    serializer_class = CustomerSerializer
+
+    def post(self, request):
+        ser = self.serializer_class(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ShopView(APIView):
+    serializer_class = ShopSerializer
+
+    def post(self, request):
+        ser = self.serializer_class(data=request.data)
+        if ser.is_valid():
+            ser.save()
+            return Response(ser.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
