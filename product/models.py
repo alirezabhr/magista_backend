97aa895ba004ec2f7d104e98180ec6c4fa96296b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-05 20:28:35 +0330

    product and product price models added

--- product/models.py
+++ product/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 
 # Create your models here.
+class Product(models.Model):
+    # shop = models.ForeignKey(Shop, models.PROTECT)
+    title = models.CharField(max_length=100, blank=True)
+    instagram_link = models.CharField(max_length=70, blank=True)
+    rate = models.PositiveSmallIntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductPrice(models.Model):
+    product = models.ForeignKey(Product, models.PROTECT)
+    price = models.IntegerField()
+    discount = models.SmallIntegerField()
+    set_at = models.DateTimeField(auto_now_add=True)
+
