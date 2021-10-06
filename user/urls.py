4f036cc04a707bd9232a7d96db01b0c1afab6db9
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-06 11:49:56 +0330

    shop and customer signup urls added

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignupUserView.as_view()),
+    path('shop/', views.ShopView.as_view()),
+    path('customer/', views.CustomerView.as_view()),
 ]
