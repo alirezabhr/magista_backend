2a774d83ef3ef4e9fa8b74263f609081492eee07
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-06 21:32:49 +0330

    login url added

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignupUserView.as_view()),
+    path('signup/', views.UserSignupView.as_view()),
+    path('login/', views.UserLoginView.as_view()),
     path('shop/', views.ShopView.as_view()),
     path('customer/', views.CustomerView.as_view()),
 ]
