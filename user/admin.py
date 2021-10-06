9f9538ff48fee28d28f243cdd14f1411b338e1bf
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-06 11:50:16 +0330

    Customer model registered in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import User, Shop
+from .models import User, Shop, Customer
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Shop)
+admin.site.register(Customer)
