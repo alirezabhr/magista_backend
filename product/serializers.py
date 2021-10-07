26acc10330ea242c5a0423d80030554277e3ccf6
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-07 15:55:27 +0330

    ProductSerializer added

--- product/serializers.py
+++ product/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
