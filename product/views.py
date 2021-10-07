fc2cdc9c0f4097e059b8e6935d373f8198d7922c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-07 16:05:21 +0330

    product detail view added

--- product/views.py
+++ product/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+class ProductDetailView(APIView):
+    serializer_class = ProductSerializer
+
+    def get(self, request, shortcode):
+        product = get_object_or_404(Product, shortcode=shortcode)
+        ser = self.serializer_class(product)
+        return Response(ser.data, status=status.HTTP_200_OK)
