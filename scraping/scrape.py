bb5c862c5a053b5ae1d1a73cc7983f5bb96fbc01
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-11 07:43:31 +0330

    login and logout methods added in Scraper to log in instagram with username and password

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from constants import *
 
@@ -23,3 +24,36 @@ class Scraper:
         self.login = False
         self.authenticated = False
         self.logged_in = False
+
+    def authenticate_with_login(self):
+        """Logs in to instagram."""
+        self.session.headers.update({'Referer': BASE_URL, 'user-agent': STORIES_UA})
+        req = self.session.get(BASE_URL)
+
+        self.session.headers.update({'X-CSRFToken': req.cookies['csrftoken']})
+
+        login_data = {'username': self.username, 'password': self.password}
+        login = self.session.post(LOGIN_URL, data=login_data, allow_redirects=True)
+        self.session.headers.update({'X-CSRFToken': login.cookies['csrftoken']})
+        self.cookies = login.cookies
+        login_text = json.loads(login.text)
+
+        if login_text.get('authenticated') and login.status_code == 200:
+            self.authenticated = True
+            self.logged_in = True
+            self.session.headers.update({'user-agent': CHROME_WIN_UA})
+            self.rhx_gis = ""
+        else:
+            logger('Login failed for ' + self.username)
+            raise Exception('problem in authenticate_with_login')
+
+    def logout(self):
+        """Logs out of instagram."""
+        if self.logged_in:
+            try:
+                logout_data = {'csrfmiddlewaretoken': self.cookies['csrftoken']}
+                self.session.post(LOGOUT_URL, data=logout_data)
+                self.authenticated = False
+                self.logged_in = False
+            except requests.exceptions.RequestException:
+                logger('Failed to log out ' + self.username)
