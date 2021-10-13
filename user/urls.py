54941b5f9e884282046c06888e3b37165e7c9e1a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-13 16:35:20 +0330

    otp urls added

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('otp/', views.OtpView.as_view()),
+    path('check-otp/', views.check_otp),
     path('signup/', views.UserSignupView.as_view()),
     path('login/', views.UserLoginView.as_view()),
     path('shop/', views.ShopView.as_view()),
