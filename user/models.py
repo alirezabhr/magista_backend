327acf007ef1d4de9a5431b3131df1709ca30e1e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-13 14:34:41 +0330

    otp model created

--- user/models.py
+++ user/models.py
@@ -60,3 +60,9 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.id}: {self.user.username}'
+
+
+class Otp(models.Model):
+    phone = models.CharField(max_length=11)
+    otp_code = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
