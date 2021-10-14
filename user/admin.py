f297696b13264550a48ffc0eecb632674a43500b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-14 19:46:50 +0330

    otp model added in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Shop, Customer
+from .models import User, Shop, Customer, Otp
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Shop)
 admin.site.register(Customer)
+admin.site.register(Otp)
