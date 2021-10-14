3777ad78083c05ddda47e4e3b1f4336cd750258d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-14 19:45:24 +0330

    send and check otp urls added

--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('otp/', views.OtpView.as_view()),
-    path('check-otp/', views.check_otp),
+    path('send-otp/', views.send_otp_view),
+    path('check-otp/', views.check_otp_view),
     path('signup/', views.UserSignupView.as_view()),
     path('login/', views.UserLoginView.as_view()),
     path('shop/', views.ShopView.as_view()),
