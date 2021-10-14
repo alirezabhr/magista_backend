a12d558c21558cfcf4d57922a99870e94c5120a5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-14 19:46:34 +0330

    created_at field format changed in otp serializer

--- user/serializers.py
+++ user/serializers.py
@@ -37,6 +37,8 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 
 class OtpSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Otp
         fields = '__all__'
