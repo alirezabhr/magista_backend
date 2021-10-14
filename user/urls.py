6dabada793cce815e85fd6e01d96088699cf7cb8
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-14 20:44:16 +0330

    user url added for changing user password and checking if the user exist or not

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('user/', views.UserView.as_view()),
     path('send-otp/', views.send_otp_view),
     path('check-otp/', views.check_otp_view),
     path('signup/', views.UserSignupView.as_view()),
