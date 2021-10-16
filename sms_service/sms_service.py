7d610351a89bba1160972a70f4e2843b3d660282
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-16 12:50:33 +0330

    sending otp with Ghasedak sms service added and works correctly

--- sms_service/sms_service.py
+++ sms_service/sms_service.py
@@ -1,14 +1,25 @@
-from kavenegar import *
+import requests
+import json
 
 
 class SMSService:
-    __api_key = '2B54526A52586736396436712F584F52445045626B6A47627A4B45454B59536444394F47787974725349413D'
-    __api = KavenegarAPI(__api_key)
-
-    def __send_sms(self, phone, text):
-        params = {'sender': '100047778', 'receptor': '09174347638', 'message': '.وب سرویس پیام کوتاه کاوه نگار'}
-        response = self.__api.sms_send(params)
-        # print(response)
+    __api_key = "11TgeUybotjKAIzarfOEJXrXYbJcKHrhGsOD/qZ9FxU"
 
     def send_otp(self, phone, otp):
-        self.__send_sms(phone, otp)
+        url = "http://api.ghasedaksms.com/v2/send/verify"
+
+        headers = {'apikey': self.__api_key}
+        body = {
+            "param1": otp,
+            "receptor": phone,
+            "type": 1,
+            "template": "magista"
+        }
+
+        response = requests.post(url, headers=headers, data=body)
+
+        if response.status_code != 200:
+            raise Exception('bad status in sending otp message', response.status_code, response.text)
+
+        if json.loads(response.text)['messageids'] < 1000:
+            raise Exception('problem in sending otp sms', response.status_code, response.text)
