ed2deb0efa98fa6215ac659657b1a66f1ff428b9
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-17 14:53:11 +0330

    name attribute added for all paths

--- user/urls.py
+++ user/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.UserView.as_view()),
-    path('send-otp/', views.send_otp_view),
-    path('check-otp/', views.check_otp_view),
-    path('signup/', views.UserSignupView.as_view()),
-    path('login/', views.UserLoginView.as_view()),
-    path('shop/', views.ShopView.as_view()),
-    path('customer/', views.CustomerView.as_view()),
-    path('user-media/', views.UserMediaView.as_view())
+    path('user/', views.UserView.as_view(), name='user-detail'),
+    path('send-otp/', views.send_otp_view, name='send-otp'),
+    path('check-otp/', views.check_otp_view, name='check-otp'),
+    path('signup/', views.UserSignupView.as_view(), name='signup'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('shop/', views.ShopView.as_view(), name='create-shop'),
+    path('customer/', views.CustomerView.as_view(), name='create-customer'),
+    path('user-media/', views.UserMediaView.as_view(), name='get-user-ig-media'),
 ]
