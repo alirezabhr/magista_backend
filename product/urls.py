244c7624be28d3ad223cea780ff7558982c4c0ff
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-27 11:05:44 +0330

    product url added

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('products/', views.ShopProductsView.as_view()),
     path('<shortcode>/', views.ProductDetailView.as_view()),
 ]
