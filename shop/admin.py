7cf7e9be5abc96c9094021c0800cc0d72b141878
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-02 14:36:10 +0330

    Shop registration in admin panel moved from user app admin to shop app admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, ProductPrice
+from .models import Shop, Product, ProductPrice
 
+admin.site.register(Shop)
 admin.site.register(Product)
 admin.site.register(ProductPrice)
--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import User, Shop, Customer, Otp
+from .models import User, Customer, Otp
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Shop)
 admin.site.register(Customer)
 admin.site.register(Otp)
