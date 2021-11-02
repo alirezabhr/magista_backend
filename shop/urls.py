8919fc3c154f4fb8b73c1ea9002578ac3f2bcf83
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-02 14:16:56 +0330

    create shop and shop preview urls added

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.ShopProductsView.as_view()),    # for vendor
-    path('shop/<str:ig_username>/products/', views.ShopProductsPreviewView.as_view()),    # for customer
-    path('<shortcode>/', views.ProductDetailView.as_view()),
+    path('<int:pk>/', views.ShopView.as_view(), name='create-shop'),   # for vendor
+    path('<int:pk>/products/', views.ShopProductsView.as_view()),    # for vendor
+    path('<str:ig_username>/products/', views.ShopProductsPreviewView.as_view()),    # for customer
+    path('preview/<str:ig_username>/', views.ShopPreview.as_view(), name='shop-preview'),    # for customer
 ]
