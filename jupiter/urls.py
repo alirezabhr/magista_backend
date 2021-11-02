93355a91f467136d1d3772f5811c7d6fa44d34f4
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-02 15:43:14 +0330

    user urls started with user/

--- jupiter/urls.py
+++ jupiter/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls')),
+    path('user/', include('user.urls')),
     path('shop/', include('shop.urls')),
 ]
 
