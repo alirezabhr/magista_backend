6d95192e564b8559287abd907f722efe490888ca
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-03 14:11:20 +0330

    shop preview url changed

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:pk>/', views.ShopView.as_view(), name='create-shop'),   # for vendor
     path('<int:pk>/products/', views.ShopProductsView.as_view()),    # for vendor
     path('<str:ig_username>/products/', views.ShopProductsPreviewView.as_view()),    # for customer
-    path('preview/<str:ig_username>/', views.ShopPreviewView.as_view(), name='shop-preview'),    # for customer
+    path('<str:ig_username>/preview/', views.ShopPreviewView.as_view(), name='shop-preview'),    # for customer
 ]
