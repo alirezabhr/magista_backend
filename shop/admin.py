47126092149df2af144d03648b9285a26febb4a6
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-03 21:27:15 +0330

    Discount registered in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Shop, Product
+from .models import Shop, Product, Discount
 
 admin.site.register(Shop)
 admin.site.register(Product)
+admin.site.register(Discount)
