4c1a4e3562ef250dfe9a8fc8e88aaced7d860200
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-08 23:05:03 +0330

    STATUS choices added for Invoice model

--- shop/models.py
+++ shop/models.py
@@ -67,9 +67,16 @@ class Discount(models.Model):
 
 
 class Invoice(models.Model):
+    class Status(models.IntegerChoices):
+        AWAITING_PAYMENT = 1
+        PAID = 2
+        SHIPPED = 3
+        RECEIVED = 4
+        CANCELED = 5
+
     shop = models.ForeignKey(Shop, models.PROTECT)
     customer = models.ForeignKey(Customer, models.PROTECT)
-    status = models.SmallIntegerField()
+    status = models.IntegerField(choices=Status.choices)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
