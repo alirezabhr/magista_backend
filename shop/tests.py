185681bbe1e63cf0f6c21ab9667c9c47460bd676
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-09 01:06:13 +0330

    test for cart api created, but it didn't work correctly

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import json
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
 
 # Create your tests here.
+class CartApiTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.cart_url = reverse('cart')
+
+    def test_cart_payment(self):
+        data = [
+            {
+                "product": 95,
+                "count": 1
+            },
+            {
+                "product": 85,
+                "count": 3
+            }
+        ]
+        # response = self.client.post(path=self.cart_url, data=json.dumps(data))
+        # self.assertEqual(response.status_code, 200)
