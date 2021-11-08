e4323bdfdb09696dd3a221f5e0145cc6c7256717
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-09 01:02:41 +0330

    Invoice and OrderItem registered in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Shop, Product, Discount
+from .models import Shop, Product, Discount, Invoice, OrderItem
 
 admin.site.register(Shop)
 admin.site.register(Product)
 admin.site.register(Discount)
+admin.site.register(Invoice)
+admin.site.register(OrderItem)
