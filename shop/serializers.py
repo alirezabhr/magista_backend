3a23be7c1096211eda934f808b687ac47a13f5fc
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-09 01:03:35 +0330

    serializers for Cart created

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Shop, Product
+from .models import Shop, Product, OrderItem, Invoice
 
 
 class ShopSerializer(serializers.ModelSerializer):
@@ -14,6 +14,7 @@ class ShopSerializer(serializers.ModelSerializer):
 class ShopPreviewSerializer(serializers.ModelSerializer):
     class Meta:
         fields = [
+            'id',
             'instagram_username',
             'province',
             'city',
@@ -32,9 +33,12 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ShopProductsPreviewSerializer(serializers.ModelSerializer):
+    shop = ShopPreviewSerializer()
+
     class Meta:
         model = Product
         fields = [
+            'id',
             'shortcode',
             'title',
             'description',
@@ -44,4 +48,44 @@ class ShopProductsPreviewSerializer(serializers.ModelSerializer):
             'discount_percent',
             'final_price',
             'is_existing',
+            'shop'
         ]
+        depth = 1
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class CartProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 1
+
+
+class CartOrderItemSerializer(serializers.Serializer):
+    product = CartProductSerializer()
+    count = serializers.IntegerField()
+
+
+class CartShopOrdersSerializer(serializers.Serializer):
+    shop_id = serializers.IntegerField()
+    orders = CartOrderItemSerializer(many=True)
+
+
+class CartSerializer(serializers.Serializer):
+    customer_id = serializers.IntegerField()
+    cart = CartShopOrdersSerializer(many=True)
+
