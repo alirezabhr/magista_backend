ea093b5635bbb55b6afa61c4f10b08cf0c3ee8c2
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-19 11:40:33 +0330

    pk names changed

--- shop/urls.py
+++ shop/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('media-query/', views.ShopMediaQueryView.as_view(), name='get-user-ig-media'),
-    path('<int:pk>/', views.ShopView.as_view(), name='create-shop'),   # for vendor
-    path('<int:pk>/products/', views.ShopProductsView.as_view()),    # for vendor
+    path('<int:vendor_pk>/', views.ShopView.as_view(), name='create-shop'),   # for vendor
+    path('<int:shop_pk>/products/', views.ShopProductsView.as_view()),    # for vendor
     path('<str:ig_username>/products/', views.ShopProductsPreviewView.as_view()),    # for customer
     path('<str:ig_username>/preview/', views.ShopPreviewView.as_view(), name='shop-preview'),    # for customer
     path('cart/', views.CartView.as_view(), name='cart'),
