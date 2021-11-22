6b50db11b446ca666134d43a4a9b58d173a95995
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-22 15:49:21 +0330

    Invoice and OrderItem registration in admin panel moved to order app

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Shop, Product, Discount, Invoice, OrderItem
+from .models import Shop, Product, Discount
 
 admin.site.register(Shop)
 admin.site.register(Product)
 admin.site.register(Discount)
-admin.site.register(Invoice)
-admin.site.register(OrderItem)
