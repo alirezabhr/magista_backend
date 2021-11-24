07d0a3ba1acd1e59f7f65da17ea86d3934829317
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 13:22:46 +0330

    invoice urls added for shop and customer

--- order/urls.py
+++ order/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
+    path('shop/<int:shop_pk>/', views.ShopInvoicesView.as_view(), name='shop-invoices'),
+    path('customer/<int:customer_pk>/', views.CustomerOrdersView.as_view(), name='customer-orders'),
 ]
