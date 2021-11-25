b70f28e9545f83aac6cd0fc2964984d5c4a2ae24
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-25 12:40:40 +0330

    payment model created

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.conf import settings
+from django.utils.timezone import localtime
 
-# Create your models here.
+
+class IPGTokenRequestPayload:
+    def __init__(self, invoice_num, invoice_date, amount, redirect_url):
+        self.invoice_number = invoice_num
+        self.invoice_date = invoice_date
+        self.terminal_code = settings.TERMINAL_CODE
+        self.merchant_code = settings.MERCHANT_CODE
+        self.amount = amount
+        self.redirect_address = f"https:magista.ir/payment/result/{redirect_url}"
+        self.time_stamp = localtime()
+        self.action = 1003
