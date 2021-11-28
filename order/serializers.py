b71652d0a6eca27acb696af23acdb504e1c3fe27
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-28 11:35:07 +0330

    PaymentDetail serializer created

--- order/serializers.py
+++ order/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from shop.models import Product
 from user.serializers import CustomerSerializer
-from .models import OrderItem, IPGPayment, Order, Invoice
+from .models import OrderItem, PaymentInvoice, Order, Invoice, PaymentDetail
 from shop.serializers import ProductSerializer
 
 
@@ -78,10 +78,24 @@ class InvoiceSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class IPGPaymentSerializer(serializers.ModelSerializer):
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+class PaymentInvoiceSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
-        model = IPGPayment
+        model = PaymentInvoice
         fields = '__all__'
+
+
+class PaymentDetailSerializer(serializers.ModelSerializer):
+    amount = serializers.ReadOnlyField()
+    paid_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
+    class Meta:
+        model = PaymentDetail
+        fields = '__all__'
+
+
+class PaymentResultSerializer(serializers.Serializer):
+    invoice_number = serializers.CharField(max_length=50)
+    invoice_date = serializers.CharField(max_length=50)
+    trx_reference_id = serializers.CharField(max_length=50)
