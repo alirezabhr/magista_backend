262fdc0fb188d13401b3e2ad2a4a994217227ec6
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-29 12:17:12 +0330

    issue view created to save issues

--- logger/views.py
+++ logger/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import IssueSerializer
+
 
 # Create your views here.
+class IssueView(APIView):
+    serializer_class = IssueSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        data = request.data
+        for item in data:
+            item['user'] = request.user.pk
+
+        ser = self.serializer_class(data=data, many=True)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+
+        for s in ser.data:
+            if s['critical']:
+                pass    # TODO send email
+
+        return Response(ser.data, status=status.HTTP_201_CREATED)
