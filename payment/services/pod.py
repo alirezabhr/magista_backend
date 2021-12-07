3e9c5cf1f05f1c4607defc50a8b7b275cb75675d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-07 22:02:44 +0330

    _create_transaction_id method changed

--- payment/services/pod.py
+++ payment/services/pod.py
@@ -2,6 +2,7 @@ import json
 import string
 import random
 from datetime import datetime
+import jdatetime
 
 import requests
 
@@ -19,7 +20,7 @@ class BankError(Exception):
 class Pod:
     API_URL = 'https://api.pod.ir/srv/sc/nzh/doServiceCall'
     PAYA_PRODUCT_ID = 1076566  # شناسه سرویس انتقال پایا
-    __PAYA_API_KEY = '5f689f253a6c4ecbbb6f4f5ab9efcfae'  # کلید وب‌سرویس انتقال پایا
+    __PAYA_API_KEY = 'fc5791e0a31e44d4a40cf9d49439513e'  # کلید وب‌سرویس انتقال پایا
     __BUSINESS_TOKEN = '2560c5533dc74b8ea007b33509a811a9'  # توکن کسب و کار
 
     def __init__(self):
@@ -31,11 +32,14 @@ class Pod:
     def _gen_time_stamp(self):
         return datetime.now().strftime('%Y/%m/%d %H:%M:%S:%f')[:-3]
 
+    def _create_transaction_date(self):
+        return jdatetime.datetime.now().date().strftime('%Y/%m/%d')
+
     def _request_builder(self, sc_product_id, sc_api_key, data):
         headers = {
-            '_token_': self.__BUSINESS_TOKEN,
-            '_token_issuer_': 1,
             'Content-Type': 'application/x-www-form-urlencoded',
+            '_token_': self.__BUSINESS_TOKEN,
+            '_token_issuer_': '1',
         }
 
         payload = {
@@ -44,7 +48,7 @@ class Pod:
             'body': json.dumps(data),
         }
 
-        response = requests.post(self.API_URL, headers=headers, data=json.dumps(payload))
+        response = requests.post(self.API_URL, headers=headers, data=payload)
         response = json.loads(response.text)
 
         if response['hasError']:
@@ -55,17 +59,14 @@ class Pod:
     def withdraw(self, amount, dest_sheba, dest_full_name, description):
         # type: (int, str, str, str) -> dict
 
-        time_stamp_millisecond = self._gen_time_stamp()
-        transaction_date = time_stamp_millisecond[:10]  # e.g. '2021/12/07'
-
         data = {
             "Amount": amount,
             "DestinationIban": dest_sheba,
             "RecieverFullName": dest_full_name,
-            "TransactionDate": transaction_date,
+            "TransactionDate": self._create_transaction_date(),
             "SourceDepNum": self.__src_deposit_num,
             "Description": description,
-            "TransactionId": self.create_transaction_id,
+            "TransactionId": self._create_transaction_id(),
             "IsAutoVerify": True,
             "SenderReturnDepositNumber": self.__src_deposit_num,
             "CustomerNumber": self.__bank_customer_id,
@@ -81,12 +82,15 @@ class Pod:
         else:
             raise BankError(result)
 
-    def create_transaction_id(self):
+    def _create_transaction_id(self):
+        org_code = '4321'
         ascii_sum = 0
-        date_time = self._gen_time_stamp()
         random_choices = string.ascii_lowercase + string.ascii_uppercase + string.digits
-        random_str = ''.join(random.choices(random_choices, k=100))
-        for c in random_str:
-            ascii_sum += ord(c)
+        random_str = ''.join(random.choices(random_choices, k=20))
+        date_time = self._gen_time_stamp()
+        data = org_code + random_str + date_time
 
-        return f"4321-{random_str}–{date_time}-{ascii_sum}"
+        for char in data:
+            ascii_sum += ord(char)
+
+        return f"{org_code}-{random_str}-{date_time}-{ascii_sum}"
