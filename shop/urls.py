9f0c2f1cd4955f52d0f090fbeb1fb95ab922e087
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-14 14:40:12 +0330

    products changed to product in endpoints

--- shop/urls.py
+++ shop/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('media-query/', views.ShopMediaQueryView.as_view(), name='get-user-ig-media'),
     path('<int:vendor_pk>/', views.ShopView.as_view(), name='create-shop'),   # for vendor
-    path('<int:shop_pk>/products/', views.ShopProductsView.as_view()),    # for vendor
+    path('<int:shop_pk>/product/', views.ShopProductsView.as_view()),    # for vendor
     path('<int:shop_pk>/bank-credit/', views.ShopBankCreditsView.as_view()),    # for vendor
-    path('<str:ig_username>/products/', views.ShopProductsPreviewView.as_view()),    # for customer
+    path('<str:ig_username>/product/', views.ShopProductsPreviewView.as_view()),    # for customer
     path('<str:ig_username>/preview/', views.ShopPublicView.as_view(), name='shop-preview'),    # for customer
     path('product/<str:product_shortcode>/', views.ProductView.as_view()),    # for vendor
     path('product/<str:product_shortcode>/preview/', views.ProductPublicView.as_view()),    # for customer
