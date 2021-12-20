6704c2947dc279d30e27408be5614aaa13eb70be
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-20 20:21:26 +0330

    str method override for Scraper model

--- scraping/models.py
+++ scraping/models.py
@@ -6,3 +6,6 @@ class Scraper(models.Model):
     password = models.CharField(max_length=30)
     is_working = models.BooleanField(default=False)
     scrape_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username} - {self.scrape_count}"
