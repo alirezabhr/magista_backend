f54212f040e497b91960064056f94b0aa55a61f5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-21 13:03:26 +0330

    remove_shop_media_directory function can remove folders and subfolders

--- utils/utils.py
+++ utils/utils.py
@@ -16,15 +16,11 @@ def is_expired_otp(otp_date_time):
         return True
 
 
-def remove_shop_media_directory(ig_username, dir_name):
-    folder_path = os.path.join(settings.MEDIA_ROOT, 'shop', ig_username, dir_name)
+def remove_shop_media_directory(ig_username, *dirs):
+    dirs_path = ''
+    for d in dirs:
+        dirs_path = os.path.join(dirs_path, d)
+    folder_path = os.path.join(settings.MEDIA_ROOT, 'shop', ig_username, dirs_path)
 
     if os.path.exists(folder_path):
         shutil.rmtree(folder_path)
-
-
-def remove_shop_media_image(ig_username, dir_name, file_name):
-    file_path = os.path.join(settings.MEDIA_ROOT, 'shop', ig_username, dir_name, file_name)
-
-    if os.path.isfile(file_path):
-        os.remove(file_path)
