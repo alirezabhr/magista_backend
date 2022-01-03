30d116338e9ad28da343d08389a8afa5769b6b2a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-03 10:35:01 +0330

    Withdraw model registered in admin panel

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from payment.models import PaymentDetail, PaymentInvoice
+from payment.models import PaymentDetail, PaymentInvoice, Withdraw
 
 # Register your models here.
 admin.site.register(PaymentDetail)
 admin.site.register(PaymentInvoice)
+admin.site.register(Withdraw)
