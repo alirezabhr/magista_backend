bd920fbef1a6972034d36a23635dda668569d70a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-22 08:15:33 +0330

    shopDiscount model registered in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Shop, BankCredit, TagLocation, Post, ProductImage, Product, ProductDiscount
+from .models import Shop, BankCredit, TagLocation, Post, ProductImage, Product, ProductDiscount, ShopDiscount
 
 admin.site.register(Shop)
 admin.site.register(BankCredit)
@@ -10,3 +10,4 @@ admin.site.register(Post)
 admin.site.register(ProductImage)
 admin.site.register(Product)
 admin.site.register(ProductDiscount)
+admin.site.register(ShopDiscount)
