5a9e1aca0afd8f72fbd56cad4ff65bf599f0e6f0
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-22 10:29:54 +0330

    OrderShopDiscount model registered in admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Invoice, Order, OrderItem
+from .models import Invoice, Order, OrderItem, OrderShopDiscount
 
 admin.site.register(Invoice)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(OrderShopDiscount)
