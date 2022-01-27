7e3efea4566de2babf5377f6809816096756df03
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-27 20:08:32 +0330

    comment added

--- order/urls.py
+++ order/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
-    path('shop/<int:shop_pk>/', views.ShopOrdersView.as_view(), name='shop-orders'),
+    path('shop/<int:shop_pk>/', views.ShopOrdersView.as_view(), name='shop-orders'),    # vendor
     path('customer/<int:customer_pk>/', views.CustomerOrdersView.as_view(), name='customer-orders'),
     path('invoice/<int:invoice_pk>/', views.InvoiceView.as_view()),
     path('<int:order_pk>/', views.OrderView.as_view()),
