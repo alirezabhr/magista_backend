20b46c5d70e2b1af81d42eb43c9a8af588de3cf2
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-31 08:29:54 +0330

    username and password added in Scraper login challenge

--- scraping/service/login_challenge.py
+++ scraping/service/login_challenge.py
@@ -1,5 +1,14 @@
-from .scrape import Scraper
+from scrape import Scraper
 
-scraper = Scraper()
-checkpoint_url = input('checkpoint url')
+username = input('username: ')
+password = input('password: ')
+
+scraper = Scraper(username, password)
+
+try:
+    scraper.authenticate_with_login()
+except Exception as e:
+    print(e)
+
+checkpoint_url = input('checkpoint url: ')
 scraper.login_challenge(checkpoint_url)
