4494a6b8ea55bb4e4a9a7e1b95d6aeeb10ff9da1
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-06 09:23:05 +0330

    user_id, nonce and is_active fields added in Scraper model

--- scraping/models.py
+++ scraping/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Scraper(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
+    user_id = models.BigIntegerField()
+    nonce = models.CharField(max_length=120, blank=True)
+    is_active = models.BooleanField(default=True)
     is_working = models.BooleanField(default=False)
     scrape_count = models.IntegerField(default=0)
 
