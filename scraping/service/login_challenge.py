3a0e826de227e5789e2c01086a30714a6872817e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-07 15:39:56 +0330

    script implemented for the times when you can not log in, and it needs a verification

--- scraping/service/login_challenge.py
+++ scraping/service/login_challenge.py
@@ -1,14 +1,29 @@
-from scrape import Scraper
+import json
 
-username = input('username: ')
-password = input('password: ')
+from scraping.service.scrape import Scraper
 
-scraper = Scraper(username, password)
 
-try:
-    scraper.authenticate_with_login()
-except Exception as e:
-    print(e)
+def run_script():
+    username = input('username: ')
+    password = input('password: ')
 
-checkpoint_url = input('checkpoint url: ')
-scraper.login_challenge(checkpoint_url)
+    scraper = Scraper(username, password)
+
+    try:
+        scraper.try_to_authenticate()
+    except Exception as e:
+        print(e)
+        checkpoint_url = input('checkpoint url: ')
+        cookies = scraper.login_challenge(checkpoint_url)
+
+        authenticated = scraper.test_authentication()
+        print('authenticated' if authenticated else 'not authenticated')
+
+        print('Cookies: ')
+        if cookies:
+            print(json.dumps(cookies.get_dict()))
+            print(json.dumps(scraper.session.cookies.get_dict()))
+            scraper.save_cookies()
+
+
+run_script()
