a9fd3b381ca9ca99587c48bab9276bf9cad38159
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-07 15:32:35 +0330

    ONE_TAP_WEB_LOGIN url added

--- scraping/service/constants.py
+++ scraping/service/constants.py
@@ -1,6 +1,7 @@
 BASE_URL = 'https://www.instagram.com/'
 LOGIN_URL = BASE_URL + 'accounts/login/ajax/'
 LOGOUT_URL = BASE_URL + 'accounts/logout/'
+ONE_TAP_WEB_LOGIN = BASE_URL + 'accounts/one_tap_web_login/'
 CHROME_WIN_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
 USER_URL = BASE_URL + '{0}/?__a=1'
 USER_INFO = 'https://i.instagram.com/api/v1/users/{0}/info/'
