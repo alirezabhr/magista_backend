ada2c48db2035f1963a903b267b87ee9bccaee4a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-12 12:49:55 +0330

    DeliveryPriceSerializer, OccasionallyFreeDeliverySerializer and ShipmentSerializer implemented

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import Shop, Product, ProductDiscount, BankCredit, ProductAttribute, ProductImage, Post, TagLocation, \
-    ShopDiscount
+    ShopDiscount, Shipment, DeliveryPrice, OccasionallyFreeDelivery
 
 
 class ShopSerializer(serializers.ModelSerializer):
@@ -152,3 +152,89 @@ class ShopDiscountSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShopDiscount
         fields = '__all__'
+
+
+class DeliveryPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryPrice
+        fields = '__all__'
+        read_only_fields = ('shipment',)
+
+
+class OccasionallyFreeDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OccasionallyFreeDelivery
+        fields = '__all__'
+        read_only_fields = ('shipment',)
+
+
+class ShipmentSerializer(serializers.ModelSerializer):
+    national_post = DeliveryPriceSerializer(allow_null=True)  # writable
+    online_delivery = DeliveryPriceSerializer(allow_null=True)  # writable
+    city_free_cost_from = OccasionallyFreeDeliverySerializer(allow_null=True)  # writable
+    country_free_cost_from = OccasionallyFreeDeliverySerializer(allow_null=True)  # writable
+
+    class Meta:
+        model = Shipment
+        fields = '__all__'
+
+    def validate_national_post(self, value):
+        if value is not None and value.get('type') != DeliveryPrice.DeliveryType.NATIONAL_POST:
+            raise serializers.ValidationError(f'type "{value.get("type")}" is not correct')
+        return value
+
+    def validate_online_delivery(self, value):
+        if value is not None and value.get('type') != DeliveryPrice.DeliveryType.ONLINE_DELIVERY:
+            raise serializers.ValidationError(f'type "{value.get("type")}" is not correct')
+        return value
+
+    def validate_city_free_cost_from(self, value):
+        if value is not None and value.get('type') != OccasionallyFreeDelivery.AreaType.CITY:
+            raise serializers.ValidationError(f'type "{value.get("type")}" is not correct')
+        return value
+
+    def validate_country_free_cost_from(self, value):
+        if value is not None and value.get('type') != OccasionallyFreeDelivery.AreaType.COUNTRY:
+            raise serializers.ValidationError(f'type "{value.get("type")}" is not correct')
+        return value
+
+    def validate(self, attrs):
+        if attrs['send_everywhere'] is True and attrs['national_post'] is None:
+            raise serializers.ValidationError('national post required when send_everywhere is True')
+        if attrs['national_post'] is None and attrs['online_delivery'] is None:
+            raise serializers.ValidationError('need to select at least one way for shipping')
+
+        if attrs['city_free_cost'] == Shipment.FreeDelivery.OCCASIONALLY_FREE and \
+                attrs['city_free_cost_from'] is None:
+            raise serializers.ValidationError('set city_free_cost_from when city_free_cost is OCCASIONALLY_FREE')
+        if attrs['country_free_cost'] == Shipment.FreeDelivery.OCCASIONALLY_FREE and \
+                attrs['country_free_cost_from'] is None:
+            raise serializers.ValidationError('set country_free_cost_from when country_free_cost is OCCASIONALLY_FREE')
+
+        return attrs
+
+    def create(self, validated_data):
+        national_post_data = validated_data.pop('national_post')
+        online_delivery_data = validated_data.pop('online_delivery')
+        city_free_cost_from_data = validated_data.pop('city_free_cost_from')
+        country_free_cost_from_data = validated_data.pop('country_free_cost_from')
+
+        shipment = Shipment.objects.create(**validated_data)
+
+        if validated_data.get('city_free_cost') == Shipment.FreeDelivery.NOT_FREE or \
+                validated_data.get('city_free_cost') == Shipment.FreeDelivery.TOTALLY_FREE:
+            city_free_cost_from_data = None
+        if validated_data.get('country_free_cost') == Shipment.FreeDelivery.NOT_FREE or \
+                validated_data.get('country_free_cost') == Shipment.FreeDelivery.TOTALLY_FREE:
+            country_free_cost_from_data = None
+
+        if city_free_cost_from_data is not None:
+            OccasionallyFreeDelivery.objects.create(shipment=shipment, **city_free_cost_from_data)
+        if country_free_cost_from_data is not None:
+            OccasionallyFreeDelivery.objects.create(shipment=shipment, **country_free_cost_from_data)
+
+        if national_post_data is not None:
+            DeliveryPrice.objects.create(shipment=shipment, **national_post_data)
+        if online_delivery_data is not None:
+            DeliveryPrice.objects.create(shipment=shipment, **online_delivery_data)
+        return shipment
