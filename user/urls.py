2fcaf4895c6dfe15493b3f1114ad249cf9af2d3b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-30 08:50:40 +0430

    refresh token url added

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
 urlpatterns = [
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.UserView.as_view(), name='user-detail'),
     path('send-otp/', views.send_otp_view, name='send-otp'),
     path('check-otp/', views.check_otp_view, name='check-otp'),
